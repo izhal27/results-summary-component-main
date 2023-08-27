415de3c3d897f5039b2a3a0c8174dfcfd30174fc
Risal Walangadi <risal.walangadi@gmail.com>
2023-08-27 08:59:21 +0800

    switch icon image to next image

--- app/components/score.tsx
+++ app/components/score.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { IScore } from "../types/score";
 
 interface Props {
@@ -15,10 +16,18 @@ function Score({ scores }: Props) {
   return (
     <ul className="sm:text-xs space-y-5 sm:space-y-3 mt-5">
       {typeOfScore.map((ts, i) => (
-        <li className={"score x-2 px-5 py-5 sm:px-3 sm:py-2 " + ts.category}>
+        <li
+          className={"score x-2 px-5 py-5 sm:px-3 sm:py-2 " + ts.category}
+          key={i}
+        >
           <div className="flex items-center">
             <span className="score-icon">
-              <img src={scores[i].icon}></img>
+              <Image
+                src={scores[i].icon}
+                alt="Category Icon"
+                layout="fill"
+                className="icon-img"
+              ></Image>
             </span>
             <p className={ts.color}>{scores[i].category}</p>
           </div>
